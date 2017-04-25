4fd23b6c004b5d8d1aa5bc3d43d0cbe0569d0b75
Ignacio Oyarzabal <ignaoya93@gmail.com>
2017-04-25 09:50:58 -0300

    Update newsAnalyzer.py

--- newsAnalyzer.py
+++ newsAnalyzer.py
@@ -2,10 +2,10 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 def analyze(URL, txt):
-    html = urlopen('https://www.yahoo.com/news/witnesses-execution-test-somber-civic-duty-200711765.html')
+    html = urlopen(URL)
     bsObj = BeautifulSoup(html)
     pureText = bsObj.findAll("p", {"type":"text"})
-    with open ("text.txt", "w") as f:
+    with open (txt, "w") as f:
         for tex in pureText:
             f.write(tex.attrs['content'])
             f.write('\n')
