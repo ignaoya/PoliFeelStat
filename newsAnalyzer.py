8e749f7f61d44acc1da5ed68a031b42b0f78ab9c
XOyarz <xavier982@hotmail.com>
2017-04-25 13:49:32 +0200

    Add files via upload

---
+++ newsAnalyzer.py
@@ -0,0 +1,13 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+
+html = urlopen('https://www.yahoo.com/news/witnesses-execution-test-somber-civic-duty-200711765.html')
+bsObj = BeautifulSoup(html)
+pureText = bsObj.findAll("p", {"type":"text"})
+with open ("text.txt", "w") as f:
+    for tex in pureText:
+        f.write(tex.attrs['content'])
+        f.write('\n')
+        f.write('\n')
+f.closed
