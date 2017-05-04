66008fe185d538bfc8fd04598a1c4e2284efcf09
XOyarz <xavier982@hotmail.com>
2017-05-04 20:32:28 +0200

    Add files via upload

--- pfs/newsAnalyzer.py
+++ pfs/newsAnalyzer.py
@@ -2,71 +2,66 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from nltk.tokenize import word_tokenize
 import sys, os
-from analyzer import Analyzer
+from pfs.analyzer import Analyzer
 
 # #imports from Tango-django page 55 to create ORM database
-#
-# from models import Article
-# from newsAnalyzer import score_arts
-#
-# import django
-# django.setup()
-# db = score_arts()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'polifeelstat.settings')
 
-# def score_arts():
-with open ("newslinks2.txt") as links:
-    lines = links.readlines()
-links.close()
-
-# initialize analyzer
-positives = os.path.join(sys.path[0], "positive-words.txt")
-negatives = os.path.join(sys.path[0], "negative-words.txt")
-analyzer = Analyzer(positives, negatives)
-
-for news_link in lines:
-    url = 'https://www.yahoo.com' + news_link
-    html = urlopen(url)
-    bsObj = BeautifulSoup(html)
-    pureText = bsObj.findAll("p", {"type":"text"})
-    with open ("text.txt", "w") as f:
-        for tex in pureText:
-            f.write(tex.attrs['content'])
-            f.write('\n')
-            f.write('\n')
-    f.closed
-
-    with open ("text.txt", "r") as f:
-        text = f.read()
-        token = word_tokenize(text)
-    f.closed
+import django
+django.setup()
+from pfs.models import Article
 
 
+def score_arts():
+    with open ("newslinks2.txt") as links:
+        lines = links.readlines()
+    links.close()
 
+    # initialize analyzer
+    positives = os.path.join(sys.path[0], "positive-words.txt")
+    negatives = os.path.join(sys.path[0], "negative-words.txt")
+    analyzer = Analyzer(positives, negatives)
 
-    #analyze tokenized article
-    score = analyzer.analyze(token)
-    length_article = len(token)
+    for news_link in lines:
+        url = 'https://www.yahoo.com' + news_link
+        html = urlopen(url)
+        bsObj = BeautifulSoup(html)
+        pureText = bsObj.findAll("p", {"type":"text"})
+        with open ("text.txt", "w") as f:
+            for tex in pureText:
+                f.write(tex.attrs['content'])
+                f.write('\n')
+                f.write('\n')
+        f.closed
 
-    db_sql = []
-    db_article = {"url": url, "score": score, "country": 0, "length": length_article}
-    print(url)
-    print(score)
-    db_sql.append(db_article)
-#return db_sql
+        with open ("text.txt", "r") as f:
+            text = f.read()
+            token = word_tokenize(text)
+        f.closed
 
-# def add_Article( urlId, feels, date, length, countryId=0):
-#     p = Article.objects.get_or_create()[0]
-#     p.urlId = db['url']
-#     p.feels = db['score']
-#     p.date = date.today()
-#     p.length = db['length_article']
-#     p.save()
-#     return p
-#
-# def shit():
-#     for art in db:
-#         add_Article(art)
-#
-# if __name__ // '__main__':
-#     shit()
+        ##To get published date use following tags(extracted from article source):
+        ##<time class="date Fz(11px) D(ib) Mb(4px)" datetime="2017-05-04T17:08:13.000Z" itemprop="datePublished" data-reactid="15">May 4, 2017</time>
+
+        #analyze tokenized article
+        score = analyzer.analyze(token)
+        length_article = len(token)
+
+        db_sql = []
+        db_article = {"url": url, "score": score, "country": 0, "length": length_article}
+        print(url)
+        print(score)
+        db_sql.append(db_article)
+        add_Article(db_article)
+
+def add_Article(art):
+    p, created = Article.objects.get_or_create(
+        countryId=0,
+        urlId=art["url"],
+        feels=art["score"],
+        length=art["length"])
+
+score_arts()
+
+if __name__ == '__main__':
+    score_arts()
 
