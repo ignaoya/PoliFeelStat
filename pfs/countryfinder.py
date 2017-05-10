c5c43aa154bc52135711e9c7b967468ff80f124e
Ignacio Oyarzabal <ignaoya93@gmail.com>
2017-05-10 16:37:20 -0300

    Update countryfinder.py

--- pfs/countryfinder.py
+++ pfs/countryfinder.py
@@ -10,12 +10,9 @@ django.setup()
 def find_country(textfile):
     with codecs.open(textfile, "r", encodings="utf-8", errors='ignore') as f:
         text = f.read()
-        countryList = []
-        for c in countries:
-            if c['name'] in text:
-                countryList.append(c['name'])
+        countryList = [c for c in countries if c['name'] in text]
         f.closed
         return countryList
 
 if __name__ == '__main__':
-    print("This module does works with newsAnalyser.py")
+    print("This module works with newsAnalyser.py")
