4cf75f4b88fcbdf280b517659af2671a25df2480
Ignacio Oyarzabal <ignaoya93@gmail.com>
2017-05-12 14:26:46 -0300

    Update countryfinder.py

--- pfs/countryfinder.py
+++ pfs/countryfinder.py
@@ -1,16 +1,16 @@
-from pfs.countryinfo import countries
+from countryinfo import countries
 import codecs
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'polifeelstat.settings')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'polifeelstat.settings')
 
-import django
-django.setup()
+#import django
+#django.setup()
 
 
 def find_country(textfile):
-    with codecs.open(textfile, "r", encodings="utf-8", errors='ignore') as f:
+    with codecs.open(textfile, "r", encoding="utf-8", errors='ignore') as f:
         text = f.read()
-        countryList = [c['name'] for c in countries if (c['name'] in text | c['capital'] in text)]
+        countryList = [c['name'] for c in countries if c['name'] in text]
         f.closed
         return countryList
 
