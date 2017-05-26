a2c2548036806673a3e1ce1e468af42417327759
XOyarz <xavier982@hotmail.com>
2017-05-26 21:52:44 +0200

    Stats 2 works for Argentina

--- pfs/views.py
+++ pfs/views.py
@@ -14,11 +14,38 @@ def about(request):
 def stats1(request):
 
     articles = Article.objects.get(id=579)
-    #flag = []
-    #lag.append(Country.objects.filter(article_id = countries.id))
+
     countries = Country.objects.filter(article_id = articles.id)
     print(articles)
     print(countries)
 
     context_dict = {'countries': countries, 'articles': articles}
     return render(request, 'pfs/stats1.html', context=context_dict)
+
+def stats2(request):
+
+    search_term = "Argentina"
+    countries = Country.objects.filter(country = search_term)
+    mentions = len(countries)
+
+    latest = Article.objects.get(urlId = countries[mentions - 1].article).date
+
+    articles = []
+    average = 0
+    avg_length = 0
+    for i in countries:
+        art = Article.objects.get(urlId = i.article)
+        print(art.feels)
+        articles.append(art)
+        average += Article.objects.get(urlId = i.article).feels
+        avg_length += Article.objects.get(urlId = i.article).length
+        print(average)
+    average /= mentions
+    average = str(round(average, 2))
+    avg_length /= mentions
+    avg_length = str(round(avg_length, 2))
+
+
+    context_dict = {'countries':countries, 'articles':articles, 'search_term':search_term,
+                    'mentions':mentions, 'latest':latest, 'average':average, 'avg_length':avg_length}
+    return render(request, 'pfs/stats2.html', context=context_dict)
